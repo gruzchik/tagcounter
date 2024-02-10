40701c6bc0128a11370a1e2c6a682cea1c0affe8
Igor Pogorelko <ahtyrka@gmail.com>
2024-02-10 16:56:31 +0200

    uncomment not used code

--- tagcounter.py
+++ tagcounter.py
@@ -30,32 +30,6 @@ class MyHTMLParser(HTMLParser):
         listing.append(tag)
 
         counter += 1
-#        print("number", counter)
-#        for attr in attrs:
-#            print("     attr:", attr)
-
-#    def handle_endtag(self, tag):
-#        print("End tag  :", tag)
-
-#    def handle_data(self, data):
-#        print("Data     :", data)
-
-#    def handle_comment(self, data):
-#        print("Comment  :", data)
-#
-#    def handle_entityref(self, name):
-#        c = unichr(name2codepoint[name])
-#        print("Named ent:", c)
-
-#    def handle_charref(self, name):
-#        if name.startswith('x'):
-#            c = unichr(int(name[1:], 16))
-#        else:
-#            c = unichr(int(name))
-#        print("Num ent  :", c)
-
-#def handle_decl(self, data):
-#    print("Decl     :", data)
 
 parser = MyHTMLParser()
 
