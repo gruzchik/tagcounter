2e1c60c1d27b43cf4b760c3316087a3805f575c8
Pro <ahtyrka@gmail.com>
2025-01-09 23:58:16 +0200

    refactoring: add htmlparser import to the top

--- tagcounter.py
+++ tagcounter.py
@@ -8,6 +8,7 @@ import sys
 from collections import Counter
 # import pandas as pd
 import requests
+from html.parser import HTMLParser
 
 print(len(sys.argv))
 if (len(sys.argv)==2) or (len(sys.argv)==3):
@@ -23,9 +24,6 @@ listing =[]
 EXISTING_TAG = False
 tagurl = sys.argv[1]
 print("URL outside", tagurl)
-
-from html.parser import HTMLParser
-#from htmlentitydefs import name2codepoint
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
